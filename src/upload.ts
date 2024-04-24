7cd717fc7d34cdff0df0b191ccf950d2b1eef7d4
ztimson <zaktimson@gmail.com>
2024-04-24 07:01:36 -0400

    Fixed upload export

--- src/upload.ts
+++ src/upload.ts
@@ -6,7 +6,7 @@ export type UploadEvents = TypedEvents & {
 	progress: (progress: number) => any;
 }
 
-function uploadProgress(files : File | FileList, url: string) {
+export function uploadProgress(files : File | FileList, url: string) {
 	const xhr = new XMLHttpRequest();
 	const progress = new TypedEmitter<UploadEvents>();
 	const formData = new FormData();
