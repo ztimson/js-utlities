069339716440fd80aaa66cf5662aff09225c6663
ztimson <zaktimson@gmail.com>
2024-07-18 23:59:54 -0400

    Added upload function with PromiseProgress support

---
+++ src/upload.ts
@@ -0,0 +1,25 @@
+import {PromiseProgress} from './promise-progress.ts';
+
+export type UploadOptions = {
+	url: string;
+	file: File;
+	headers?: {[key: string]: string};
+	withCredentials?: boolean;
+}
+
+export function uploadWithProgress(options: UploadOptions) {
+	return new PromiseProgress((res, rej, prog) => {
+		const xhr = new XMLHttpRequest();
+		const formData = new FormData();
+		formData.append('file', options.file);
+
+		xhr.withCredentials = !!options.withCredentials
+		Object.entries(options.headers || {}).forEach(([key, value]) => xhr.setRequestHeader(key, value));
+		xhr.upload.addEventListener('progress', (event) => event.lengthComputable ? prog(event.loaded / event.total) : null);
+		xhr.upload.addEventListener('load', (resp) => res(resp));
+		xhr.upload.addEventListener('error', (err) => rej(err));
+
+		xhr.open('POST', options.url);
+		xhr.send(formData);
+	});
+}
