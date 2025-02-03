fe9fdb9384ce997915153647037ceab21f5f109b
ztimson <zaktimson@gmail.com>
2025-02-03 14:20:18 -0500

    Fixed blackorwhite invalid argument crash

--- src/color.ts
+++ src/color.ts
@@ -4,7 +4,7 @@
  * @return {"white" | "black"} Color with the most contrast
  */
 export function blackOrWhite(background: string): 'white' | 'black' {
-	const exploded = background.match(background.length >= 6 ? /\w\w/g : /\w/g);
+	const exploded = background?.match(background.length >= 6 ? /\w\w/g : /\w/g);
 	if(!exploded) return 'black';
 	const [r, g, b] = exploded.map(hex => parseInt(hex, 16));
 	const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
