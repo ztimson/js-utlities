e3bbd13ed8981af818e84c03a182236588f11a95
ztimson <zaktimson@gmail.com>
2025-06-16 23:37:02 -0400

    Removed log statement that snuck in

--- src/cache.ts
+++ src/cache.ts
@@ -34,10 +34,7 @@ export class Cache<K extends string | number | symbol, T> {
 		if(options.storageKey && !options.storage && typeof(Storage) !== 'undefined') options.storage = localStorage;
 		if(options.storage) {
 			if(options.storage instanceof Table) {
-				(async () => (await options.storage?.getAll()).forEach((v: any) => {
-					console.log(v);
-					this.add(v)
-				}))()
+				(async () => (await options.storage?.getAll()).forEach((v: any) => this.add(v)))()
 			} else if(options.storageKey) {
 				const stored = options.storage?.getItem(options.storageKey);
 				if(stored != null) try { Object.assign(this.store, JSON.parse(stored)); } catch { }
