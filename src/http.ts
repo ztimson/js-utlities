b3223661dd87dd391ad394d5b3940416b05f5868
ztimson <zaktimson@gmail.com>
2025-07-22 14:22:47 -0400

    More efficient regex

--- src/http.ts
+++ src/http.ts
@@ -78,7 +78,7 @@ export class Http {
 		if(!this.url && !opts.url) throw new Error('URL needs to be set');
 		let url = opts.url?.startsWith('http') ? opts.url : (this.url || '') + (opts.url || '');
 		url = url.replaceAll(/([^:]\/)\/+/g, '$1');
-		if(opts.fragment) url.includes('#') ? url.replace(/#.*(\?|\n)/g, (match, arg1) => `#${opts.fragment}${arg1}`) : `${url}#${opts.fragment}`;
+		if(opts.fragment) url.includes('#') ? url.replace(/#.*([?\n])/g, (match, arg1) => `#${opts.fragment}${arg1}`) : `${url}#${opts.fragment}`;
 		if(opts.query) {
 			const q = Array.isArray(opts.query) ? opts.query :
 				Object.keys(opts.query).map(k => ({key: k, value: (<any>opts.query)[k]}))
